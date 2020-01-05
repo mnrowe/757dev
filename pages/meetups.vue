3e8c108fc8b85458fb51770f9e88d2e53bf567a8
Matt <mrowe.w@gmail.com>
2020-01-04 21:48:38 -0500

    added api call, but waiting on Oauth2 from meetup

--- pages/meetups.vue
+++ pages/meetups.vue
@@ -3,17 +3,29 @@
 <template>
   <div class="test">
     <NavBar />
-    <b-container></b-container>
+    <b-container>
+      <p></p>
+    </b-container>
   </div>
 </template>
 
 <script>
+// import axios from 'axios'
 import NavBar from '~/components/NavBar'
+
+// const URL = 'https://api.meetup.com'
 
 export default {
   components: {
     NavBar
   }
+
+  // Waiting on Oauth2 Conformation
+  /* fetch({ store, params }) {
+    return axios.get(`${URL}/find/upcoming_events`).then((res) => {
+      console.log(store)
+    })
+  } */
 }
 </script>
 
