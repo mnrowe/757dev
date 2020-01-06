f912e8372ac719facce35c2547d13a3f7c946f12
Matt <mrowe.w@gmail.com>
2020-01-05 20:04:30 -0500

    make about page visiible

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBar from '~/components/NavBar'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  height: 100vh;
+  background-color: var(--dark);
+}
+</style>
