0e710684011a6b86237ab16598e98182fac9b257
Matt <mrowe.w@gmail.com>
2020-01-06 20:57:38 -0500

    space out code of conduct

--- pages/code-of-conduct.vue
+++ pages/code-of-conduct.vue
@@ -17,20 +17,25 @@
         appearance, body size, race, religion, sexual images, deliberate
         intimidation, stalking, sustained disruption of channels or other
         mediums, and unwelcome sexual attention. Participants asked to stop any
-        harassing behavior are expected to comply immediately. Sponsors are also
-        subject to the anti-harassment policy. In particular, sponsors should
-        not use sexualized images, activities, or other material. Slack staff
-        (including volunteers) should not use sexualized content or otherwise
-        create a sexualized environment. If a participant engages in harassing
-        behavior, the Slack group organizers may take any action they deem
-        appropriate, including warning the offender or expulsion from the Slack
-        group. If you are being harassed, notice that someone else is being
-        harassed, or have any other concerns, please contact a member of the
-        Slack group staff immediately. Slack group staff can be identified
-        through the Team Directory. Slack group staff will be happy to help
-        participants contact local law enforcement or otherwise assist those
-        experiencing harassment to feel safe for the duration of their time in
-        our Slack group. We value your contribution.
+        harassing behavior are expected to comply immediately.
+      </p>
+      <p>
+        Sponsors are also subject to the anti-harassment policy. In particular,
+        sponsors should not use sexualized images, activities, or other
+        material. Slack staff (including volunteers) should not use sexualized
+        content or otherwise create a sexualized environment. If a participant
+        engages in harassing behavior, the Slack group organizers may take any
+        action they deem appropriate, including warning the offender or
+        expulsion from the Slack group.
+      </p>
+      <p>
+        If you are being harassed, notice that someone else is being harassed,
+        or have any other concerns, please contact a member of the Slack group
+        staff immediately. Slack group staff can be identified through the Team
+        Directory. Slack group staff will be happy to help participants contact
+        local law enforcement or otherwise assist those experiencing harassment
+        to feel safe for the duration of their time in our Slack group. We value
+        your contribution.
       </p>
     </b-container>
   </div>
