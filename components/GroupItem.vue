e0bce0f3c4ac8442c8486266b4d9aa991fbe9793
mnrowe <mrowe.w@gmail.com>
2020-01-19 13:56:22 -0500

    add highlight to group item component

--- components/GroupItem.vue
+++ components/GroupItem.vue
@@ -1,14 +1,26 @@
 <template>
-  <b-list-group-item class="flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">{{ meetup.name }}</h5>
-    </div>
+  <b-list-group-item
+    :href="meetup.meetUpUrl"
+    variant="dark"
+    class="flex-column align-items-start"
+  >
+    <b-container>
+      <b-row>
+        <b-col cols="10">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ meetup.name }}</h5>
+          </div>
+          <p class="mb-1">
+            {{ meetup.about }}
+          </p>
 
-    <p class="mb-1">
-      {{ meetup.about }}
-    </p>
-
-    <small>{{ meetup.location }}</small>
+          <small>{{ meetup.location }}</small>
+        </b-col>
+        <b-col>
+          <b-img :src="meetup.logo" fluid rounded="circle"></b-img>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-list-group-item>
 </template>
 
@@ -19,5 +31,3 @@ export default {
   }
 }
 </script>
-
-<style scoped></style>
